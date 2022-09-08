fe5746e0eea2c1ccd7d2b156e89a83762d87cb9f
Hasan Demirtas <hasan.tayip.demirtas@gmail.com>
2022-09-08 14:22:10 +0300

    Add project files.

---
+++ HasanDemirtasReaMiniTask/StudentProfessorTest .cs
@@ -0,0 +1,15 @@
+﻿using HasanDemirtasReaMiniTask;
+
+Person person = new Person();
+person.Greet();
+
+Student student = new Student();
+student.SetAge(21);
+student.Greet();
+student.ShowAge();
+student.Study();
+
+Professor professor = new Professor();
+professor.SetAge(58);
+professor.Greet();
+professor.Explain();
